509617d87f094953337160d3f5dbad6955831f14
BoredUser <iamrahultambe.com>
2021-10-14 20:28:54 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+// require( './data/' );
+
+const io = require("socket.io")((3000), { cors: true });
+
+let drawing;
+io.on("connection", socket => {
+
+    socket.on("join-room", (room) => {
+		socket.join(room);
+		socket.emit("joined");
+        socket.emit("drawing", drawing);
+	});
+
+	socket.on("drawing", (data) => {
+        if (data.action === "draw"){
+            drawing = data.data;
+            socket.to(data.room).emit("drawing", data.data);
+        }
+        else if(data.action === "clear"){
+            drawing = null;
+            socket.to(data.room).emit("clear");
+        }
+		
+	});
+
+    socket.on("checkAnswer", (data) =>{
+        console.log(data);
+    });
+    
+});
