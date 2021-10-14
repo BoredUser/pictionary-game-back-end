99238b16d402b64de263d1ac88ea6f5dee7a38ca
BoredUser <iamrahultambe.com>
2021-10-14 22:55:35 +0530

    Express and folder structure

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 // require( './data/' );
 
-require( './data/init' );
+require( './data/index' );
 
 const express = require( 'express' );
 const path = require( 'path' );
 const cors = require( 'cors' );
 
 const app = express();
-
+var http = require("http").createServer(app)
+var io = require("socket.io")(http, {  cors: {    origin: "*",    methods: ["GET", "POST"]  }})
 
 // middlewares
-
 app.use( cors() );
 app.use( express.urlencoded( { extended: false } ) );
 app.use( express.json() );
@@ -18,7 +18,37 @@ app.use( express.json() );
 
 const PORT = process.env.PORT || 3000;
 
-app.listen( PORT, error => {
+// socket events 
+
+let drawing;
+io.on("connection", socket => {
+
+    socket.on("join-room", (room) => {
+		socket.join(room);
+		socket.emit("joined");
+        socket.emit("drawing", drawing);
+	});
+
+	socket.on("drawing", (data) => {
+        if (data.action === "draw"){
+            drawing = data.data;
+            socket.to(data.room).emit("drawing", data.data);
+        }
+        else if(data.action === "clear"){
+            drawing = null;
+            socket.to(data.room).emit("clear");
+        }
+		
+	});
+
+    socket.on("checkAnswer", (data) =>{
+        console.log(data);
+    });
+    
+});
+
+
+http.listen( PORT, error => {
     if( error ) {
         console.error( error.message );
         return;
@@ -27,7 +57,7 @@ app.listen( PORT, error => {
     console.log( `Check http://localhost:${PORT}` );
 });
 
-require ('./socket/index');
+
 
 
 
