cd0545fe8c152bd6f37c97e18de6920978b5f713
BoredUser <iamrahultambe.com>
2021-10-27 16:27:39 +0530

    -

--- src/data/init.js
+++ src/data/init.js
@@ -11,13 +11,10 @@ if( process.env.NODE_ENVIRONMENT !== 'production' ) {
     console.log( 'Connecting to mongodb://localhost:27017/workshopsDB' );
     mongoose.connect( 'mongodb://localhost:27017/pictionaryGameDB' );
 } else {
-    console.log( "password: ", process.env.DB_PASSWORD );
-    console.log( "Username: ", process.env.DB_USER );
-    const string = `mongodb+srv://rahulTambe:Qpalzmqm@cluster0.nznal.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    const string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
     //const connection = `mongodb+srv://${process.env.DB_USER}:/${process.env.DB_PASSWORD}@cluster0.nznal.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
     mongoose.connect( string );
 }
-
 
 
 mongoose.connection.on( 'connected', () => {
